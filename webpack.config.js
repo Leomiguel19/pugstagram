97eadbeed9a8e55f36f7bfbfaf55c87c5cfeba2a
Leonardo Guilarte <leomiguel1907@gmail.com>
2023-04-07 23:27:39 -0400

    Local development server

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,38 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: './src/index.js',
+    entry: "./src/index.js",
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js',
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js",
     },
     resolve: {
-        extensions: ['*', '.mjs', '.js', '.svelte'],
+        extensions: ["*", ".mjs", ".js", ".svelte"],
     },
     module: {
         rules: [
-            {
-                test: /\.js?$/,
-                exclude: /node_modules/,
-                use:{
-                    loader: 'babel-loader',
-                },
+        {
+            test: /\.js?$/,
+            exclude: /node_modules/,
+            use: {
+                loader: "babel-loader",
             },
-            {
-                test: /\.svelte$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'svelte-loader'
-                }
+        },
+        {
+            test: /\.svelte$/,
+            exclude: /node_modules/,
+            use: {
+            loader: "svelte-loader",
             },
+        },
         ],
-        plugins: [
-            new HtmlWebpackPlugin({
-                inject: true,
-                template: './public/index.html',
-                filename: '/index.html'
-            })
-        ]
     },
-}
+    plugins: [
+        new HtmlWebpackPlugin({
+        inject: true,
+        template: "./public/index.html",
+        filename: "./index.html",
+        }),
+    ],
+    };
