2ddf60935449c24380c0cca22dbf37fde353c98f
Leonardo Guilarte <leomiguel1907@gmail.com>
2023-04-08 21:31:47 -0400

    structure

--- src/containers/App.svelte
+++ src/containers/App.svelte
@@ -1,10 +1,20 @@
-<style>
-    :global(body){
-        background-color: #676778;
-        color: white;
-    }
-</style>
+<script>
+  import Header from "../components/Header.svelte";
+  import Main from "../components/Main.svelte";
+  import Sidebar from "../components/Sidebar.svelte";
+  import TimeLine from "../components/TimeLine.svelte";
+</script>
 
-<div class="main">
-    <h1>Hello Pugstagram</h1>
-</div>
+
+<Header />
+<Main>
+  <TimeLine />
+  <Sidebar />
+</Main>
+
+<style>
+  :global(body) {
+    background-color: #676778;
+    color: white;
+  }
+</style>
